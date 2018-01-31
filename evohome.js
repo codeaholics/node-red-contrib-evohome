1af6f7928b8de44285e6b003ba376143a8e0af98
Danny Yates <danny@codeaholics.org>
2018-01-31 11:00:15 +0000

    Extremely rudimentary HGI80-format decoder

--- evohome.js
+++ evohome.js
@@ -74,8 +74,6 @@ module.exports = function(RED) {
     function EvohomeIn(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        var context = node.context();
-
         node.server = RED.nodes.getNode(config.server);
         if (node.server) {
             node.log('ensuring connection');
@@ -100,4 +98,38 @@ module.exports = function(RED) {
         }
     }
     RED.nodes.registerType('evohome-out', EvohomeOut);
+
+    function EvohomeHGIDecoder(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function(msg) {
+            const fields = msg.payload.split(/\s+/);
+            if (fields.length != 9) {
+                node.warn('Failed to parse HGI80 format message: ' + msg.payload);
+                return;
+            }
+
+            msg.payload = {
+                original: msg.payload,
+                decoded: {
+                    unk0: fields[0],
+                    type: fields[1],
+                    unk1: fields[2],
+                    addr: [
+                        fields[3],
+                        fields[4],
+                        fields[5]
+                    ],
+                    cmd: fields[6],
+                    len: fields[7],
+                    payload: {
+                        raw: fields[8],
+                        bytes: Buffer.from(fields[8], 'hex')
+                    }
+                }
+            };
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType('evohome-hgi-decoder', EvohomeHGIDecoder);
 }
