c7d08750135529076c7fc5e86afe1aca5f6c2562
Danny Yates <danny@codeaholics.org>
2018-02-02 14:20:45 +0000

    Temporarily disable parsing of know errored messages

--- src/nodes/evohome-hgi-parser.js
+++ src/nodes/evohome-hgi-parser.js
@@ -3,6 +3,9 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         const node = this;
         node.on('input', (msg) => {
+            // For the time being, ignore messages with *ERR in them as there are quite a lot of them
+            if (msg.payload.indexOf('*ERR') !== -1) return;
+
             const fields = msg.payload.split(/\s+/);
             if (fields.length !== 9) {
                 node.warn(`Failed to parse HGI80 format message: ${msg.payload}`);
