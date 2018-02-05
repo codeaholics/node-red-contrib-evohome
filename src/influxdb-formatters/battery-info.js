0576c0148dc485b1c914fd993f995cb497a3e96a
Danny Yates <danny@codeaholics.org>
2018-02-05 21:03:09 +0000

    Don't write a literal "undefined" to InfluxDB

--- src/influxdb-formatters/battery-info.js
+++ src/influxdb-formatters/battery-info.js
@@ -2,11 +2,11 @@ module.exports = function(d) {
     return {
         measurement: 'BatteryInfo',
         tags: {
-            device: d.device.addr,
-            deviceType: d.device.type,
-            deviceName: d.device.name,
-            zone: d.device.zone,
-            zoneName: d.device.zoneName
+            device: d.device.addr || '',
+            deviceType: d.device.type || '',
+            deviceName: d.device.name || '',
+            zone: d.device.zone || '',
+            zoneName: d.device.zoneName || ''
         },
         values: {
             batteryLevel: d.batteryLevel
--- src/influxdb-formatters/heat-demand.js
+++ src/influxdb-formatters/heat-demand.js
@@ -2,12 +2,12 @@ module.exports = function(d) {
     return {
         measurement: 'HeatDemand',
         tags: {
-            subsystem: d.subsystem,
-            device: d.device.addr,
-            deviceType: d.device.type,
-            deviceName: d.device.name,
-            zone: d.device.zone,
-            zoneName: d.device.zoneName
+            subsystem: d.subsystem || '',
+            device: d.device.addr || '',
+            deviceType: d.device.type || '',
+            deviceName: d.device.name || '',
+            zone: d.device.zone || '',
+            zoneName: d.device.zoneName || ''
         },
         values: {
             demand: d.demand
