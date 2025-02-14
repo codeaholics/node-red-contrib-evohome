05d50120b79c69812bb0be2613561229d9b798bb
Danny Yates <danny@codeaholics.org>
2025-02-14 10:50:41 +0000

    Attempt to fix config finder

--- src/nodes/evohome-config-finder.js
+++ src/nodes/evohome-config-finder.js
@@ -1,4 +1,5 @@
 const Address = require('../address');
+const Config = require('../config');
 
 const ADDRESS_REGEX = /\b\d{2}:\d{6}\b/g;
 
@@ -24,21 +25,18 @@ module.exports = function(RED) {
         const node = this;
 
         const configNode = RED.nodes.getNode(n.config);
-        node.config = JSON.parse(configNode.json);
+        node.config = new Config(JSON.parse(configNode.json));
 
         node.on('input', (msg) => {
-            // eslint-disable-next-line arrow-body-style
-            const addresses = findAddresses(msg.payload).filter((elem, pos, arr) => {
-                return arr.indexOf(elem) === pos;
-            });
-
-            addresses.map(addr => new Address(addr, node.config)).forEach((addr) => {
-                if (!addr.isConfigured() && !addr.isSiteController()) {
-                    node.send({
-                        payload: addr.describe()
-                    });
-                }
-            });
+            /* eslint-disable dot-location */
+            findAddresses(msg.payload).
+                filter((elem, pos, arr) => arr.indexOf(elem) === pos).
+                map(addr => new Address(addr, node.config)).
+                filter(addr => !addr.isConfigured() && !addr.isSiteController()).
+                forEach(addr => node.send({
+                    payload: addr.describe()
+                }));
+            /* eslint-enable dot-location */
         });
     }
     RED.nodes.registerType('evohome-config-finder', EvohomeConfigFinder);
